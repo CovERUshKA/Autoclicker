a3dc9f787a8320e7fe040800796355f06ab39808
CovERUshKA <43516399+CovERUshKA@users.noreply.github.com>
2021-02-02 20:00:38 +0300

    Update Def.hpp for COGUI

--- Autoclicker GUI/Autoclicker GUI/GUI/Def.hpp
+++ Autoclicker GUI/Autoclicker GUI/GUI/Def.hpp
@@ -10,3 +10,36 @@ typedef BOOL(*pfnCOGUIWndProc)
 	UINT message,
 	void* pStruct
 	);
+
+struct COGUI_DropListReceive
+{
+	std::wstring pStr;
+};
+
+struct COGUI_ElementCreateStruct
+{
+	UINT elementID;
+	LPCWSTR lpElementName;
+	FLOAT x;
+	FLOAT y;
+	FLOAT nWidth;
+	FLOAT nHeight;
+	UINT uiParams;
+	INT ID;
+};
+
+enum COGUI_ElementType
+{
+	// Header
+	COGUI_MinimizeButton = 1,
+	COGUI_ScaleButton = 2,
+	COGUI_CloseButton = 3,
+
+	// Client area
+	COGUI_Button = 4,
+	COGUI_TextEdit = 5,
+	COGUI_TextLabel = 6,
+	COGUI_RecordButton = 7,
+	COGUI_Table = 8,
+	COGUI_DropList = 9,
+};
